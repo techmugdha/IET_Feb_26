8c818b3b79326f9348292527f3c794915acf2f33
Tech Mugdha <the.tech.mugdha@gmail.com>
2026-06-15 13:16:17 +0530

    Add project files.

---
+++ OldDotNetProjectTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OldDotNetProjectTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Bye bye");
+
+        }
+    }
+}
